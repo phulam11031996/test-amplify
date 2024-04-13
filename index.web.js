07a1003d85d43814eedc81210e081aefd3a9cb19
phulam11031996 <phulam11031996@gmail.com>
2024-04-12 18:19:34 -0700

    trouble shoot deploying on amplify

--- index.web.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry } from "react-native";
-import name from "./app.json";
-import App from "./App";
-
-AppRegistry.registerComponent(name, () => App);
-AppRegistry.runApplication(name, {
-  initialProps: {},
-  rootTag: document.getElementById("root"),
-});
